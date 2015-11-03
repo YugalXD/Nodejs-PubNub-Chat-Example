6045635bc42262343876eff66c6333acdee3ac12
Yugal Yadav <YugalXD@users.noreply.github.com>
2015-11-03 18:02:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ var pubnub = require("pubnub")({
     publish_key   : "pub-c-####################################", // Your Publish Key
     subscribe_key : "sub-c-####################################"  // Your Subscriber Key
 });
-
 app.get('/', function(req, res) {
     var express = require('express');
     app.use(express.static(path.join(__dirname)));
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-
 // Register events on socket connection
 io.on('connection', function(socket) {
     socket.on('chatMessage', function(chroom, from, msg) {
@@ -24,7 +22,6 @@ io.on('connection', function(socket) {
         io.emit('chatMessage', chroom, from, msg);
     });
 });
-
 // Listen application request on port 3000
 http.listen(3000, function() {
     console.log('listening on *:3000');
